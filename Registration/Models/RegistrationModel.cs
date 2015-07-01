6e3fd695a41a406dfd672e3499d4395f6036f949
Ahsan Rocky <vampxlr@yahoo.com>
2015-07-01 21:28:13 +0600

    first commit

---
+++ Registration/Models/RegistrationModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Registration.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string HearAbout { get; set; }
+        [Required]
+        public string PhoneNo { get; set; }
+        [Required]
+        public string SkypeID { get; set; }
+        [Required]
+        public string Area { get; set; }
+        [Required]
+        public string Instituition { get; set; }
+        [Required]
+        public string FacebookURL { get; set; }
+    }
+}
